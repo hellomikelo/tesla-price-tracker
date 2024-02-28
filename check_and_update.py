5af07e510272b0a71ee05f40f0d0cda3dc25f667
yuanhunglo <lo.yuanhung@gmail.com>
2024-02-27 21:07:48 -0800

    First commit.

---
+++ check_and_update.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+import shutil
+import json
+import os
+
+def are_json_files_equal(file1_path, file2_path):
+    """Check if 2 json files are the same."""
+    # Read the content of the first JSON file
+    with open(file1_path, 'r') as file1:
+        content1 = file1.read()
+
+    # Read the content of the second JSON file
+    with open(file2_path, 'r') as file2:
+        content2 = file2.read()
+
+    # Parse the JSON content into Python objects
+    data1 = json.loads(content1)
+    data2 = json.loads(content2)
+
+    # Compare the Python objects for equality
+    return data1 == data2
+        
+
+def copy_json(source_path, destination_path):
+    """Copy source file to destination file."""
+    shutil.copyfile(source_path, destination_path)
+    print(f'File copied from {source_path} to {destination_path}')
+
+
+def send_to_discord(file_path):
+    """Use Discord bot to send results to channel."""
+    
+    with open(file_path, 'r') as file:
+        content = file.read()
+    res = json.loads(content)
+
+    min_price = min(res, key=lambda x: x['purchase_price'])['purchase_price']
+
+    # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+    bot = discord.Client()
+
+    # Event triggered when the bot is ready
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user.name} ({bot.user.id})')
+        channel = bot.get_channel(1084864988688154627) 
+        embed = discord.Embed(
+            title='🚘 Lowest Tesla MYLR price update!',
+            description=f'Lowest purchase price is **${min_price:,}**',
+            color=discord.Color.blue()
+        )
+        
+        # Add fields to the embed
+        for _res in res: 
+            if _res['purchase_price'] == min_price:
+                value_str = f"**After tax credit:** ${_res['after_tax_credit_price']:,}\n**Odometer:** {_res['odometer']} miles\n**Base features:** {_res['base_features']}\n**URL:** {_res['url']}"
+                embed.add_field(name=f"🚗 ${_res['price_adjustment']:,} ({_res['pct_discount']}%) adjustment", value=value_str, inline=False)
+        
+        await channel.send(embed=embed)
+        await bot.close()
+
+    # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
+    bot.run(os.getenv('DISCORD_TOKEN'))
+
+
+if __name__ == '__main__':
+    snapshot_path = 'snapshot_prices.json'
+    latest_path = 'latest_prices.json'
+
+    if not are_json_files_equal(latest_path, snapshot_path):
+        # Send latest result to Discord
+        print("Send latest info to Discord.")
+        send_to_discord(latest_path)
+
+        # Replace snapshot json
+        print("Replace snapshot with latest file.")
+        copy_json(latest_path, snapshot_path)
+        
---
+++ fetch_data.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+import re
+import json
+import selenium
+
+def get_data():
+    # Initialize Chrome Selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-gpu')
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537'
+    options.add_argument('user-agent={0}'.format(user_agent))
+
+    driver = webdriver.Chrome(options=options) 
+    driver.set_page_load_timeout(90)
+
+    # Load the URL and get the page source
+    URL = 'https://www.tesla.com/inventory/new/my?TRIM=LRAWD&WHEELS=NINETEEN&CABIN_CONFIG=FIVE&arrangeby=plh&zip=94043&range=0'
+    driver.get(URL)
+
+    results_container = WebDriverWait(driver, 100).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "results-container"))
+    )
+
+    sections = results_container.find_elements(By.CSS_SELECTOR, "#iso-container > div > div.inventory-app-wrapper.tds-scrim--white > main > div > article")
+
+    # Parse section for car info
+    car_info = []
+    for section in sections:
+        # Get raw HTML
+        purchase_price_str = section.find_element(By.CLASS_NAME, "result-purchase-price").get_attribute("innerHTML")
+        try: 
+            base_price_str = section.find_element(By.CLASS_NAME, "result-price-base-price").get_attribute("innerHTML")
+        except Exception as e:
+            # If there's no discount
+            base_price_str = purchase_price_str
+        after_credit_price_str = section.find_element(By.CSS_SELECTOR, '#iso-container > div > div.inventory-app-wrapper.tds-scrim--white > main > div > article > section.result-federal-incentive > div > span').get_attribute("innerHTML")
+        try: 
+            odometer_str = section.find_element(By.CSS_SELECTOR, "#iso-container > div > div.inventory-app-wrapper.tds-scrim--white > main > div > article > section.result-header > div.result-basic-info > div.tds-text--caption").get_attribute("innerHTML")
+        except Exception as e:
+            # If there's no miles driven
+            odometer_str = '0'
+        base_features_str = section.find_element(By.CSS_SELECTOR, '#iso-container > div > div.inventory-app-wrapper.tds-scrim--white > main > div > article > section.result-features.features-grid > ul.result-regular-features.tds-list.tds-list--unordered').get_attribute("innerHTML")
+        car_type = section.find_element(By.CLASS_NAME, "tds-text_color--10").get_attribute("innerHTML")
+
+        # Parse raw HTML
+        purchase_price = int(purchase_price_str.replace('$', '').replace(',', ''))
+        base_price = int(base_price_str.replace('$', '').replace(',', ''))
+        after_credit_price = int(re.findall(r'\b\d+\b', after_credit_price_str.replace('$', '').replace(',', ''))[0])
+        base_features = re.findall(r'<li.*?>(.*?)<\/li>', base_features_str)
+        odometer = int(re.findall(r'\b\d+\b', odometer_str.replace('$', '').replace(',', ''))[0])
+        url = 'https://www.tesla.com/my/order/7SAY' + section.get_attribute('data-id').replace('-search-result-container', '')
+
+        assert car_type == 'Model Y Long Range Dual Motor All-Wheel Drive', "Car is not a Model Y!"
+
+        car_info.append({
+            'purchase_price': purchase_price,
+            'after_tax_credit_price': after_credit_price,
+            'odometer': odometer,
+            'base_price': base_price,
+            'price_adjustment': base_price - purchase_price,
+            'pct_discount': round((base_price - purchase_price) / base_price * 100, 2),
+            'base_features': base_features,
+            'url': url
+        })
+
+    # Sort records based on purchase price and odometer 
+    car_info = sorted(car_info, key=lambda x: (x['purchase_price'], x['odometer']))
+    min_price = min(car_info, key=lambda x: x['purchase_price'])['purchase_price']
+    [print(x) for x in car_info if x['purchase_price'] == min_price]
+
+    # Write out results
+    with open("latest_prices.json", "w") as f:
+        # Dump the list to the file
+        json.dump(car_info, f)
+
+if __name__ == '__main__':
+    get_data()
